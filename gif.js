f8aa5afd40b0e3917ee29f6ddb68642961dba63f
Wim Mostmans <wim@sitebase.be>
2014-07-18 17:47:50 +0200

    add gif response and uuid

---
+++ gif.js
@@ -0,0 +1,11 @@
+/**
+ * Respond to a request with an empty GIF
+ * @param  {Response} res
+ * @return {void}
+ */
+function gif( res ) {
+	var imgHex = '47494638396101000100800000dbdfef00000021f90401000000002c00000000010001000002024401003b';
+	var imgBinary = new Buffer(imgHex, 'hex');
+	res.writeHead(200, {'Content-Type': 'image/gif' });
+	res.end(imgBinary, 'binary');
+}
